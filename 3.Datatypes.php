f4b8486a87ba501feec5b28fbf322aef023a904d
Pedro Cisneros Santana <37348419+PedroCisnerosSantana@users.noreply.github.com>
2020-04-16 18:12:23 +0200

    Create 3.Datatypes.php

---
+++ 3.Datatypes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $int = 30;
+    $string = "String";
+    $float = 30.50;
+    echo strlen($string);
+    echo ("First character of $string is: " . $string[0]);
+    ?>
+</body>
+</html>
